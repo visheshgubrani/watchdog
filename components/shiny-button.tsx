11cd79ffc7e70866aee4c74b1abe2c905e77bc76
Vishesh <vishesh.gubrani@gmail.com>
2025-02-26 19:45:24 +0530

    completed heading section

--- components/shiny-button.tsx
+++ components/shiny-button.tsx
@@ -3,13 +3,13 @@ import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 import { AnchorHTMLAttributes } from 'react'
 
-interface ButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement> {}
-export const Button = ({
+interface ShinyButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement> {}
+export const ShinyButton = ({
   className,
   children,
   href,
   ...props
-}: ButtonProps) => {
+}: ShinyButtonProps) => {
   return (
     <Link
       href={href ?? '#'}
@@ -19,11 +19,14 @@ export const Button = ({
       )}
       {...props}
     >
-      <span className="relative z-10 flex items-center gap-2">
+      <span className='relative z-10 flex items-center gap-2'>
         {children}
-        <ArrowRight className="size-4 shrink-0 text-white transition-transform duration-300 ease-in-out group-hover:translate-x-[2px]" />
+        <ArrowRight className='size-4 shrink-0 text-white transition-transform duration-300 ease-in-out group-hover:translate-x-[2px]' />
       </span>
-      <div className="ease-[cubic-bezier(0.19,1,0.22,1)] absolute -left-[75px] -top-[50px] -z-10 h-[155px] w-8 rotate-[35deg] bg-white opacity-20 transistion-all duration-500 group-hover:left-[120%]" />
+      <div
+        className='ease-[cubic-bezier(0.19,1,0.22,1)] absolute -left-[75px] -top-[50px] -z-10 h-[155px] w-8 rotate-[35deg]
+       bg-white opacity-20 transistion-all duration-500 group-hover:left-[120%]'
+      />
     </Link>
   )
 }
